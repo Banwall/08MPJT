9794296e1f059884e5af59834300d7bbfe312f0b
win10 <win10@192.168.219.101>
2021-06-03 14:03:37 +0900

    product 구현시작 / 업데이트 메소드 오류 / 06-03

--- 08.Model2MVCShop(RestFull Server)/src/main/java/com/model2/mvc/web/product/ProductRestController.java
+++ 08.Model2MVCShop(RestFull Server)/src/main/java/com/model2/mvc/web/product/ProductRestController.java
@@ -1,65 +1,60 @@
 package com.model2.mvc.web.product;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.model2.mvc.common.Page;
-import com.model2.mvc.common.Search;
-import com.model2.mvc.service.domain.User;
-import com.model2.mvc.service.user.UserService;
-
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
 
 //==> 회원관리 RestController
 @RestController
 @RequestMapping("/product/*")
 public class ProductRestController {
 	
-	///Field
 	@Autowired
-	@Qualifier("userServiceImpl")
-	private UserService userService;
-	//setter Method 구현 않음
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
 		
 	public ProductRestController(){
 		System.out.println(this.getClass());
 	}
 	
-	@RequestMapping( value="json/getUser/{userId}", method=RequestMethod.GET )
-	public User getUser( @PathVariable String userId ) throws Exception{
+	@RequestMapping( value="json/getProduct/{prodNo}",method=RequestMethod.GET)
+	public Product getProduct( @PathVariable int prodNo) throws Exception {
 		
-		System.out.println("/user/json/getUser : GET");
+		System.out.println("/product/json/getProduct : GET");
 		
-		//Business Logic
-		return userService.getUser(userId);
+		return productService.getProduct(prodNo);
 	}
-
-	@RequestMapping( value="json/login", method=RequestMethod.POST )
-	public User login(	@RequestBody User user,
-									HttpSession session ) throws Exception{
 	
-		System.out.println("/user/json/login : POST");
-		//Business Logic
-		System.out.println("::"+user);
-		User dbUser=userService.getUser(user.getUserId());
+	@RequestMapping( value="json/getProduct",method=RequestMethod.POST)
+	public Product getProduct( @RequestBody Product product) throws Exception {
 		
-		if( user.getPassword().equals(dbUser.getPassword())){
-			session.setAttribute("user", dbUser);
-		}
+		System.out.println("/product/json/getProduct : POST");
 		
-		return dbUser;
+		return productService.getProduct(product.getProdNo());
+	}
+	
+	@RequestMapping(value="json/addProduct",method=RequestMethod.POST)
+	public Product addProduct( @RequestBody Product product)throws Exception {
+		
+		System.out.println("/product/json/addProduct : POST");
+		
+		productService.addProduct(product);
+
+		return product;
+	}
+	
+	@RequestMapping(value="json/updateProductView/{prodNo}",method=RequestMethod.GET)
+	public Product updateProduct( @PathVariable int prodNo)throws Exception {
+		
+		System.out.println("/product/json/updateProductView : GET ");
+		
+		return productService.getProduct(prodNo);
 	}
 }
